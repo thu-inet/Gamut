d1c1afc4406aa1eae7d3a7830eced561850943ee
albertzhang <albert.zhangweij@outlook.com>
2023-12-25 11:18:49 +0800

    remove the track of tests and older_files folders

--- older_files/Spectrum_analysis/Peak_area/for WY.py
+++
@@ -1,6 +0,0 @@
-import spectrum
-
-filepath=r"C:\\Users\\ALber\\Desktop\\Inp2.spe"
-list_erg,list_eff=spectrum.read(filepath)
-
-print(str(spectrum.areaTPA(list_erg,list_eff,peak=305,avg_number=5,width_base=5,base_method=0)))  
